4b1c600c2282526eacb15a2256080dff98d864bb
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-01-25 21:21:01 +0300

    init

---
+++ src/main/java/com/example/sem4SpringHomework/Sem4SpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem4SpringHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4SpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4SpringHomeworkApplication.class, args);
+	}
+
+}
